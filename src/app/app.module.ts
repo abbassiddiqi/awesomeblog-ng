d0b15a9a98e21fcfd455a1ccbed1727b25913967
Abbas Siddiqi <abbas.siddiqi@yahoo.com>
2018-04-05 17:58:43 +0500

    Add and configure @angular flex-layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
@@ -25,6 +26,7 @@ import { PostsService } from './posts.service';
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
+    FlexLayoutModule,
     MatToolbarModule,
     MatCardModule,
     MatPaginatorModule
